09b9462edc337de57bfd48f0c20841a0facce01a
Kazuki Tobita <kazukiti201@gmail.com>
2024-03-23 19:47:28 +0900

    Add signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'user_id',
         'password',
     ];
 
@@ -29,19 +29,5 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get("/hello", function(){
-    return response()->json(["message" => "hello world!"]);
+Route::post('/auth/signup', function(Request $request) {
+    $user = User::create([
+        'name' => $request->name,
+        'user_id' => $request->user_id,
+        'password' => Hash::make($request->password),
+    ]);
+
+    $token = $user->createToken('token')->plainTextToken;
+
+    return response()->json(['token' => $token], 201);
+});
+
+Route::get('/hello', function() {
+    return response()->json(['message' => 'hello world!']);
 });
