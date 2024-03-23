84caa1d2e1a828e9008ff8cc74eb0a2a9cecd18d
Kazuki Tobita <kazukiti201@gmail.com>
2024-03-24 06:34:17 +0900

    Add route grouping

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,10 @@ Route::get('/hello', function() {
     return response()->json(['message' => 'hello world!']);
 });
 
-Route::post('/auth/signup', [AuthController::class, 'signup']);
-Route::post('/auth/signin', [AuthController::class, 'signin']);
+Route::prefix('auth')->group(function() {
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/signin', [AuthController::class, 'signin']);
+});
 
 Route::get('/user', function (Request $request) {
     return response()->json($request->user());
