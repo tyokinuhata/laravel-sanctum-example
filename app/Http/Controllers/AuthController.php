0d4e09d3a6cb31d5acd58fded91400764f33fc3b
Kazuki Tobita <kazukiti201@gmail.com>
2024-03-24 06:32:15 +0900

    Add controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'user_id' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:8|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'user_id' => $request->user_id,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    public function signin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|string|max:255',
+            'password' => 'required|string|min:8|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = User::where('user_id', $request->user_id)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,58 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
-
-use App\Models\User;
 
 Route::get('/hello', function() {
     return response()->json(['message' => 'hello world!']);
 });
 
-Route::post('/auth/signup', function(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|string|max:255',
-        'user_id' => 'required|string|max:255|unique:users',
-        'password' => 'required|string|min:8|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return response()->json(['errors' => $validator->errors()], 400);
-    }
-
-    $user = User::create([
-        'name' => $request->name,
-        'user_id' => $request->user_id,
-        'password' => Hash::make($request->password),
-    ]);
-
-    $token = $user->createToken('token')->plainTextToken;
-
-    return response()->json(['token' => $token], 201);
-});
-
-Route::post('/auth/signin', function(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'user_id' => 'required|string|max:255',
-        'password' => 'required|string|min:8|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return response()->json(['errors' => $validator->errors()], 400);
-    }
-
-    $user = User::where('user_id', $request->user_id)->first();
-
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        return response()->json(['message' => 'Unauthorized'], 401);
-    }
-
-    $token = $user->createToken('token')->plainTextToken;
-
-    return response()->json(['token' => $token], 200);
-});
+Route::post('/auth/signup', [AuthController::class, 'signup']);
+Route::post('/auth/signin', [AuthController::class, 'signin']);
 
 Route::get('/user', function (Request $request) {
     return response()->json($request->user());
